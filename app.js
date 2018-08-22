2db2f14511811dc42c6ebff75e7357b259288940
Eddie Ruiz <dev.blacknode@gmail.com>
2018-08-21 20:15:38 -0500

    make it better

--- app.js
+++ app.js
@@ -9,16 +9,18 @@ var authenticate = require('./authenticate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ciudadanoRouter = require('./routes/ciudadanoRouter');
-var hechoDelictivoRouter=require('./routes/hechoDelictivoRouter');
+var hechoDelictivoRouter = require('./routes/hechoDelictivoRouter');
 var mongoose = require('mongoose');
 var config = require('./config');
 var db = mongoose.connect(config.mongoUrl);
-mongoose.connection.on('error',()=>{console.log("base de datos en problemas")})
-mongoose.connection.once('open',()=>{console.log("Conectado")})
+mongoose.connection.on('error', () => { console.log("base de datos en problemas") })
+mongoose.connection.once('open', () => { console.log("Conectado") })
+var cors = require('cors')
 
 var app = express();
 
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -33,37 +35,36 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ciudadano', ciudadanoRouter);
-app.use('/hecho_delictivo',hechoDelictivoRouter)
+app.use('/hecho_delictivo', hechoDelictivoRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
-function auth (req, res, next) {
-  console.log(req.user);
+function auth(req, res, next) {
+    console.log(req.user);
 
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');                          
-    err.status = 401;
-    next(err);
-  }
-  else {
+    if (!req.user) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+    } else {
         next();
-  }
+    }
 }
 
 module.exports = app;
